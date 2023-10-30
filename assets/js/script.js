91839f025aebe0c967ed64ff3be213eec2274b73
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-10-30 13:41:20 +0000

    Add code to check answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded",function(){
     for(let button of buttons){ //loop through buttons in page 
         button.addEventListener('click',function(){ // add event listener to each button
             if(this.getAttribute("data-type") === "submit"){ // check data-type attribue for submit
-                alert("You clicked Submit");
+                checkAnswer();
             }else{
                 let gameType = this.getAttribute("data-type"); // to alert diplay button clicked data type
                 runGame(gameType);
@@ -32,11 +32,37 @@ function runGame(gameType){
 
 };
 
-
+/**
+ * checks the answer against the first element
+ * then returned calculator answer array
+ */
 function checkAnswer(){
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calanswer =calculateCorrectAnswer();
+    let isCorrect = calanswer[0] === userAnswer;
 
+    if(isCorrect){
+        alert("Hey you got it right :)")
+    }else{
+        alert("Awww .. You answered ",userAnswer,"but correct answer was",calanswer[0]);
+    }
+    runGame(calanswer[1])
 };
+
+/** *
+ * get the operands and the oprator(plus ,minus etc)
+ * directly from the Dome, and returns the correct answer.
+ */
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand1").innerText);
+    let operator = document.getElementById('operator').innerText;
+    if(operator === "+"){
+        return [operand1 + operand2,"addition"];
+    }else{
+        alert("unImplemeneted operator ",operator);
+        throw  `unimplemented operator ${operator}`;
+    }
 
 };
 function incrementScore(){
@@ -45,23 +71,23 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(operand1,operand2){
- document.getElementById("operand1").textContent = operand1;
- document.getElementById("operand2").textContent = operand2;
+function displayAdditionQuestion(num1,num2){
+ document.getElementById("operand1").textContent = num1;
+ document.getElementById("operand2").textContent = num2;
  document.getElementById("operator").textContent = "+";
 
 
 };
-function displaySubtractQuestion(operand1,operand2){
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+function displaySubtractQuestion(num1,num2){
+    document.getElementById("operand1").textContent = num1;
+    document.getElementById("operand2").textContent = num2;
     document.getElementById("operator").textContent = "-";
 
 
 };
-function displayMultiQuestion(operand1,operand2){
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+function displayMultiQuestion(num1,num2){
+    document.getElementById("operand1").textContent = num1;
+    document.getElementById("operand2").textContent = num2;
     document.getElementById("operator").textContent = "X";
 
 
