b48d036e085648f65a0d7777fc4649460bc3ff0f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-10-30 12:49:32 +0000

    Add code to calculate two random numbers for the game Qs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,29 @@
 // get dom elements  and buttons event listener
 document.addEventListener("DOMContentLoaded",function(){
     let buttons = document.getElementsByTagName('button');
-    for(let button of buttons){
-        button.addEventListener('click',function(){
-            if(this.getAttribute("data-type") === "submit"){
+    for(let button of buttons){ //loop through buttons in page 
+        button.addEventListener('click',function(){ // add event listener to each button
+            if(this.getAttribute("data-type") === "submit"){ // check data-type attribue for submit
                 alert("You clicked Submit");
             }else{
-                let gameType = this.getAttribute("data-type");
+                let gameType = this.getAttribute("data-type"); // to alert diplay button clicked data type
                 alert (`You Clicked ${gameType}`);
             }
         });
     }
 })
 
+/**
+ * The main game "loop ",clalled when the script is first loaded
+ * and after user's answer has been processed
+ */
 function runGame(){
+    let num1 = Math.floor(Math.random()*25)+1;
+    let num2 = Math.floor(Math.random()*25)+1;
 
 };
+
+
 function checkAnswer(){
 
 };
